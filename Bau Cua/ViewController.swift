5095e8e6c7541642da95dca2a94e58dd0bc5e257
Phuc Nguyen <kufuc2604@gmail.com>
2019-07-12 16:34:03 -0400

    Complete Bau Cua App

--- Bau Cua/ViewController.swift
+++ Bau Cua/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView3: UIImageView!
+    
+    let diceArray = ["Bau", "Cua", "Tom", "Ca", "Huou", "Ga"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateDicesImage()
     }
 
+    @IBAction func rungButtonPressed(_ sender: UIButton) {
+            updateDicesImage()
+    }
 
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDicesImage()
+    }
+    
+    func updateDicesImage(){
+        diceImageView1.image = UIImage(named: diceArray[Int.random(in: 0...5)])
+        diceImageView2.image = UIImage(named: diceArray[Int.random(in: 0...5)])
+        diceImageView3.image = UIImage(named: diceArray[Int.random(in: 0...5)])
+    }
 }
 
