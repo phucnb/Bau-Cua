bee0ea222c21bc67edc7ea29df1f26e027b9273e
Phuc Nguyen <kufuc2604@gmail.com>
2019-07-12 15:10:26 -0400

    Initial Commit

---
+++ Bau Cua/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bau Cua
+//
+//  Created by Ba Phuc Nguyen on 2019-07-12.
+//  Copyright © 2019 Phuc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
